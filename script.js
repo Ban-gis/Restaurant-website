09a37048e20ecb87c7fd7934f6c6a69c5211e40f
Ban-gis <musa69686@gmail.com>
2025-05-22 20:25:24 -0700

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+const slider = document.querySelector('.slider');
+  const leftArrow = document.querySelector('.arrow.left');
+  const rightArrow = document.querySelector('.arrow.right');
+  const totalSlides = document.querySelectorAll('.review').length;
+
+  let currentSlide = 0;
+
+  function updateSlider() {
+    slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+  }
+
+  leftArrow.addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateSlider();
+  });
+
+  rightArrow.addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateSlider();
+  });
